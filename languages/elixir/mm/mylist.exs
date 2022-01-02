7797365c986017336272b7c2a226f787357ad2b0
goldsami <zolotarenkomax@gmail.com>
2022-01-02 13:03:59 +0200

    feat(languages/elixir): mylist module added

---
+++ languages/elixir/mm/mylist.exs
@@ -0,0 +1,47 @@
+defmodule MyList do
+  def len([]), do: 0
+  def len([_head | tail]), do: 1 + len(tail)
+
+  def map([], _func), do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
+
+  def reduce([], value, _) do
+    value
+  end
+
+  def reduce([head | tail], value, func) do
+    reduce(tail, func.(head, value), func)
+  end
+
+  def sum(list), do: _sum(list, 0)
+  defp _sum([], total), do: total
+  defp _sum([head | tail], total), do: _sum(tail, head + total)
+
+  def mapsum(list, func) do
+    map(list, func)
+    |> reduce(0, &(&1 + &2))
+  end
+
+  defp check_max(a, b) when a > b, do: a
+  defp check_max(a, b) when a <= b, do: b
+
+  def max([head | tail]) do
+    reduce(tail, head, &check_max/2)
+  end
+
+  # ceasar cipher
+  # ?a == 97, ?z == 122
+  defp _caesar_add(ch, n) when ch in ?a..?z do
+    rem(ch - ?a + n, 26) + ?a
+  end
+
+  defp _caesar_add(ch, n) when ch in ?A..?Z do
+    rem(ch - ?A + n, 26) + ?A
+  end
+
+  def caesar([], _), do: ''
+
+  def caesar([head | tail], number_to_add) do
+    [_caesar_add(head, number_to_add) | caesar(tail, number_to_add)]
+  end
+end
