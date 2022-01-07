24ce13e37d89d629532b29d74885cc87c59ba57f
goldsami <zolotarenkomax@gmail.com>
2022-01-07 18:23:50 +0200

    feat(languages/rust): guessing_game finished

--- languages/rust/guessing_game/src/main.rs
+++ languages/rust/guessing_game/src/main.rs
@@ -1,12 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Enter your variant");
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Enter your variant");
 
-    io::stdin().read_line(&mut guess).expect("Wrong input");
+        let mut guess = String::new();
 
-    println!("You entered {} number", guess);
+        io::stdin().read_line(&mut guess).expect("Wrong input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You entered {} number", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your number is too small!"),
+            Ordering::Greater => println!("Your number is too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
