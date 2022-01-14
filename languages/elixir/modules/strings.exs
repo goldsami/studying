176c3d8a63153fa8794ae01e7bbcb2eb88031255
goldsami <zolotarenkomax@gmail.com>
2022-01-14 23:16:40 +0200

    feat(languages/elixir): center words fun added

--- languages/elixir/modules/strings.exs
+++ languages/elixir/modules/strings.exs
@@ -4,4 +4,18 @@ defmodule Strings do
   end
 
   def anagram(word1, word2), do: Enum.sort(word1) == Enum.sort(word2)
+
+  def center(words) do
+    target_width = max_len(words)
+    IO.puts(target_width)
+
+    Enum.map(words, &String.ljust(&1, trunc(target_width / 2)))
+    |> Enum.map(&String.rjust(&1, target_width))
+    |> Enum.each(&IO.puts(&1))
+  end
+
+  defp max_len(words) do
+    Enum.map(words, &String.length(&1))
+    |> Enum.max()
+  end
 end
