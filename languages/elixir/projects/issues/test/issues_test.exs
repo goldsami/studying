22ad5398152c0047975a5eea784d0fed14a8971d
goldsami <zolotarenkomax@gmail.com>
2022-01-16 15:12:51 +0200

    feat(languages/elixir): cli module added to issues project

--- languages/elixir/projects/issues/test/issues_test.exs
+++ languages/elixir/projects/issues/test/issues_test.exs
@@ -1,8 +1,7 @@
 defmodule IssuesTest do
   use ExUnit.Case
-  doctest Issues
 
-  test "greets the world" do
-    assert Issues.hello() == :world
+  test "the truth" do
+    assert(true)
   end
 end
