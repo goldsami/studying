169a8347b80c44fbd6ebabe76858697c97e57382
goldsami <zolotarenkomax@gmail.com>
2022-12-05 08:42:13 +0200

    feat(languages/rust): struct test added

--- languages/rust/adder/src/lib.rs
+++ languages/rust/adder/src/lib.rs
@@ -2,6 +2,19 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +23,19 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 }
