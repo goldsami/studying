d7f5b8daea63fd846dbe52c505a59bec34a7aa7f
goldsami <zolotarenkomax@gmail.com>
2022-12-07 19:33:09 +0200

    refactor(languages/rust/minigrep): error handling changed

--- languages/rust/minigrep/src/main.rs
+++ languages/rust/minigrep/src/main.rs
@@ -1,15 +1,19 @@
-use std::{env, fs};
-use minigrep::Config;
+use minigrep::{run, Config};
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
